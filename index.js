70d110de09eb076b1f52ebe22882a9eda3a6a0b4
Dusty Wood Saunders <dustywsaunders@gmail.com>
2019-02-05 16:22:34 +0100

    sequalize revised

--- index.js
+++ index.js
@@ -2,11 +2,9 @@ const express = require('express');
 const app = express(); // app is technically your server
 
 const Sequelize = require('sequelize')
-const sequelize = new Sequelize('postgres://postgres:secret@localhost:5432/postgres', {
-  define: {
-    timestamps: false
-  }
-})
+
+const connectionString = process.env.DATABASE_URL || 'postgres://postgres:secret@localhost:5432/postgres'
+const sequelize = new Sequelize(connectionString, {define: { timestamps: false }})
 
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
